2de97a96bd173a4fa5939a4ff679ee71bb0f18b3
theshauryasingh <shauryasingh96@gmail.com>
2023-06-13 20:22:17 +0530

    closure partial;

--- Week-3/session-10/closure/closure.js
+++ Week-3/session-10/closure/closure.js
@@ -1,3 +1,17 @@
 function counter() {
-    
+    return 
 }
+
+const firstCounter = counter()
+const secondCounter = counter()
+
+// loop 5 times
+const firstValues = [];
+firstCounter();
+
+//loop 3 times
+const secondValues = [];
+secondCounter();
+
+console.log(firstValues);
+console.log(secondValues);
