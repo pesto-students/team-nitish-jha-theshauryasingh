7907505c019aacfa737af1f804586bd73d688703
theshauryasingh <shauryasingh96@gmail.com>
2023-06-18 14:35:22 +0530

    number game completed;

--- Week-3/session-9/number_guessing_game/script.js
+++ Week-3/session-9/number_guessing_game/script.js
@@ -1,7 +1,43 @@
-function playGuessingGame(numToGuess, totalGuesses) {
-    let person = prompt("Enter a number between 1 and 100");
-    console.log(person);
+function validateNumber(number, numToGuess) {
+    // console.log('validateNumber.. ', number, numToGuess);
+    if (number == numToGuess){
+        return 0;
+    }
+    else{
+        if(isNaN(number)){
+            return 1 ;
+        }
+        else if(number < numToGuess){
+            return 2;
+        }
+        else if(number > numToGuess){
+            return 3
+        }
+    }
 }
 
-playGuessingGame(10, 4);
-playGuessingGame(5);
+function playGuessingGame(numToGuess, totalGuesses=10) {
+    let numGuesses = 0;
+    let number = Number(prompt("Enter a number between 1 and 100"));
+    // console.log(numToGuess, totalGuesses, number, numGuesses);
+    while (numGuesses < totalGuesses) {
+        numGuesses++ ; // totalGuesses--;
+        verdict = validateNumber(number, numToGuess)
+        if (verdict == 0 ){
+            return numGuesses;
+        }
+        else if (verdict == 1 && numGuesses < totalGuesses){
+            number = prompt("Please enter a number");
+        }
+        else if (verdict == 2 && numGuesses < totalGuesses) {
+            number = prompt("X is too small. Guess a larger number.");
+        }
+        else if (verdict == 3 && numGuesses < totalGuesses) {
+            number = prompt("X is too large. Guess a smaller number.");0
+        }
+    }
+    return 0;
+}
+
+//console.log(playGuessingGame(10, 4));
+//console.log(playGuessingGame(5));
