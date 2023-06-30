e195eb85ae156e6efcdcae7a79acfe50f9f0ca99
theshauryasingh <shauryasingh96@gmail.com>
2023-06-30 23:31:57 +0530

    completed;

--- Week-4/session-11/js_game_object/script.js
+++ Week-4/session-11/js_game_object/script.js
@@ -1,5 +1,18 @@
 // Your solution goes here
+let game = {};
+game['lives'] = 3;game['coins'] = 0;
+console.log(game);
+Object.defineProperty(game, 'points', {
+  get: function() {    return (this.coins * 10);
+  },});
+Object.defineProperty(game, 'playerDies', {
+    value: function(){        if (this.lives >0){
+            this.lives = this.lives -1 ;        }
+    }});
 
+Object.defineProperty(game, 'newGame', {    value: function(){
+        this.lives = 3;        this.coins = 0;
+    }});
 
 console.log("lives = " + game.lives);    // should be 3
 console.log("coins = " + game.coins);    // should be 0
